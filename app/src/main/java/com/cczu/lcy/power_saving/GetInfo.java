8dfdfb72a152d361d71fa40406200f281c26027a
LiChangYong6 <1315657491@qq.com>
2018-01-14 21:54:11 +0800

    1.0
    create new xml file and new entity

---
+++ app/src/main/java/com/cczu/lcy/power_saving/GetInfo.java
@@ -0,0 +1,14 @@
+package com.cczu.lcy.power_saving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 13156 on 2018/1/14.
+ */
+
+public class GetInfo {
+
+    List<Info> infolist = new ArrayList<Info>();
+
+}
---
+++ app/src/main/java/com/cczu/lcy/power_saving/Info.java
@@ -0,0 +1,45 @@
+package com.cczu.lcy.power_saving;
+
+/**
+ * Created by 13156 on 2018/1/14.
+ */
+
+public class Info {
+    private String name;
+    private String number;
+    private String times;
+    private String bytes;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setTimes(String times) {
+        this.times = times;
+    }
+
+    public void setBytes(String bytes) {
+        this.bytes = bytes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getTimes() {
+        return times;
+    }
+
+    public String getBytes() {
+        return bytes;
+    }
+}
